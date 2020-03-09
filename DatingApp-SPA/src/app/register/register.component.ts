d13db899102cc0610230dab31962c1895619c038
Niren <niren@synthesis.co.za>
2020-03-09 11:08:56 +0200

    end of section 4

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {  } from 'protractor';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('Registration Successful');
+    }, error => {
+      console.log(error);
+    });
+    console.log(this.model);
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('Cancelled');
+  }
+}
